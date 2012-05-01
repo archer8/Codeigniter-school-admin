419e25a3531f655fc05bdc765ee3ba2a387440f8
Christopher Ganly <chrisganly@gmail.com>
2012-05-01 12:31:03 +1000

    added tabletools to repo

---
+++ tabletools/attempt.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<script type="text/javascript" charset="utf-8">
+    $('#example').dataTable( {
+            "sDom": 'T<"clear">lfrtip',
+            "oTableTools": {
+                "sSwfPath": "media/swf/copy_cvs_xls_pdf.swf"
+            }
+        } );
+    } );
+</script>
